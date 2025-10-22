efce0df84dcb550ec37ddab72ee4dd0447962da8
Arlen Resende <arlen.resende@gmail.com>
2025-10-21 21:39:09 -0300

    refactor: move auth and home components to modules directory
    Restructure component organization by moving auth, home, register, and auth-layout components to a dedicated modules/pages directory for better project organization and maintainability

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,20 @@
-import { AuthComponent } from './pages/auth/auth.component'
+import { AuthComponent } from './modules/pages/auth/auth.component'
 import { Routes } from '@angular/router'
 
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./pages/auth/auth.component').then((mod) => mod.AuthComponent),
+    loadComponent: () =>
+      import('./modules/pages/auth/auth.component').then((mod) => mod.AuthComponent),
   },
   {
     path: 'home',
-    loadComponent: () => import('./pages/home/home.component').then((mod) => mod.HomeComponent),
+    loadComponent: () =>
+      import('./modules/pages/home/home.component').then((mod) => mod.HomeComponent),
   },
   {
     path: 'register',
     loadComponent: () =>
-      import('./pages/register/register.component').then((mod) => mod.RegisterComponent),
+      import('./modules/pages/register/register.component').then((mod) => mod.RegisterComponent),
   },
 ]
