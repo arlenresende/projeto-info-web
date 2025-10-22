91fafd1b982fdebbdb8065469c4a1a5b44612d48
Arlen Resende <arlen.resende@gmail.com>
2025-10-22 00:26:19 -0300

    feat(components): add card, header and sidebar components
    Refactor home component to use new components for better maintainability and code organization

--- src/app/modules/pages/home/home.component.ts
+++ src/app/modules/pages/home/home.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { SidebarComponent } from '../../../components/sidebar/sidebar.component'
+import { HeaderComponent } from '../../../components/header/header.component'
+import { CardComponent } from '../../../components/card/card.component'
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [SidebarComponent, HeaderComponent, CardComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
